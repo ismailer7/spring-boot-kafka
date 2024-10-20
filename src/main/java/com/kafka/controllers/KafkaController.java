bfbc1af64424db4f3cee00c4688a9a1464710923
Ismail Rzouki <ismailrzouki7@gmail.com>
2024-10-20 12:49:22 +0100

    spring boot kafka

---
+++ src/main/java/com/kafka/controllers/KafkaController.java
@@ -0,0 +1,24 @@
+package com.kafka.controllers;
+
+
+import com.kafka.producer.config.MessageProducer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1")
+@RestController
+@RequiredArgsConstructor
+public class KafkaController {
+
+    private final MessageProducer messageProducer;
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestParam("message") String message) {
+        messageProducer.sendMessage("my-topic", message);
+        return "Message sent: " + message;
+    }
+
+}
